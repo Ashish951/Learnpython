e175ee28349859e2880ad2f842fff4d53ff014e1
Ashish951 <68660416+Ashish951@users.noreply.github.com>
2026-04-22 17:30:03 +0000

    added int concept

--- py.py
+++ py.py
@@ -1,1 +1,75 @@
 print("hello")
+
+# Integer
+num=3  #int assignment
+print(num)
+
+# Addition
+num2=2
+add=num+num2
+print(add)
+
+# substraction
+
+num3=4
+sub=add-num3
+print(sub)
+
+#multiplicaton
+num4=5
+mul=sub*num4
+print(mul)
+
+# division
+num5=2
+div=mul/num5  # I5t return value in float kif we use single slash
+print (div)
+
+print(type (mul))
+type(div)
+
+# floor division
+fdiv=mul//num5  # It return value in int if we use double slash
+print (fdiv)
+
+type(fdiv)
+
+# casting int
+num6=int(5.5)
+print(num6)
+
+# casting string into int
+num7=int('3')
+print(num7)
+type(num7)
+
+# single quote and double quote
+#Both are same
+text1="Hi Iam ashish"
+print(text1)
+
+text2='Hi I am ashish'
+print(text2)
+
+# but when use like below show error
+text3='I'am ashish'
+print(text3)
+
+# Then we have to use escape so it will Don’t treat the next character normally — treat it specially
+text3='I\'am ashish'
+print(text3)    
+
+# For multiline we need to use three times single or double quote
+
+text = """This is
+multiple lines"""
+print(text)
+
+text7= ''' this is multipal 
+line'''
+print(text7)
+
+
+
+
+
